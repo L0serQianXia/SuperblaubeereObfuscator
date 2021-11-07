f3c4b4ae56b38040864c880a60efd72326278c79
QianXia <1928650539@qq.com>
2021-11-07 21:48:06 +0800

    add BadAttribute crasher to crash ASM(was fixed in Recaf)

--- obfuscator-core/src/main/java/me/superblaubeere27/jobf/processors/CrasherTransformer.java
+++ obfuscator-core/src/main/java/me/superblaubeere27/jobf/processors/CrasherTransformer.java
@@ -18,14 +18,41 @@ import me.superblaubeere27.jobf.utils.NameUtils;
 import me.superblaubeere27.jobf.utils.values.BooleanValue;
 import me.superblaubeere27.jobf.utils.values.DeprecationLevel;
 import me.superblaubeere27.jobf.utils.values.EnabledValue;
+import org.objectweb.asm.Attribute;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
 
+import javax.naming.directory.Attributes;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CrasherTransformer implements IClassTransformer {
     private static final String EMPTY_STRINGS;
+
+    private Attribute BOOTSTRAP_METHODS = DummyAttribute("BootstrapMethods");
+    private Attribute MODULE_MAIN_CLASS = DummyAttribute("ModuleMainClass");
+    private Attribute NEST_HOST;
+
+    {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(2);
+            new DataOutputStream(byteArrayOutputStream).writeInt(6);
+            NEST_HOST = DummyAttribute("NestHost", byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Attribute CODE = DummyAttribute("Code");
+    private Attribute CONSTANT_VALUE = DummyAttribute("ConstantValue");
+    private Attribute STACKMAP = DummyAttribute("StackMap");
 
     static {
         StringBuilder stringBuilder = new StringBuilder();
@@ -40,6 +67,7 @@ public class CrasherTransformer implements IClassTransformer {
     private EnabledValue enabled = new EnabledValue("Crasher", DeprecationLevel.GOOD, false);
     private BooleanValue invalidSignatures = new BooleanValue("Crasher", "Invalid Signatures", "Adds invalid signatures", DeprecationLevel.GOOD, true);
     private BooleanValue emptyAnnotation = new BooleanValue("Crasher", "Empty annotation spam", "Adds annotations which are repeated newline", DeprecationLevel.GOOD, true);
+    private BooleanValue badAttribute = new BooleanValue("Crasher", "Bad Attribute", "Adds bad attribute to classes, methods, fields to break ASM", DeprecationLevel.GOOD, true);
     private JObfImpl inst;
 
     public CrasherTransformer(JObfImpl inst) {
@@ -72,7 +100,63 @@ public class CrasherTransformer implements IClassTransformer {
             });
         }
 
+        if (badAttribute.getObject()) {
+            if (node.attrs == null) {
+                node.attrs = new ArrayList<>();
+            }
+            if (!versionAtLeast(node, Opcodes.V1_7)) {
+                node.attrs.add(BOOTSTRAP_METHODS);
+            } else if (!versionAtLeast(node, Opcodes.V9)) {
+                node.attrs.add(MODULE_MAIN_CLASS);
+            } else if (!versionAtLeast(node, Opcodes.V11)) {
+                node.attrs.add(NEST_HOST);
+            }
+
+            node.fields.forEach(field -> {
+                if (field.attrs == null) {
+                    field.attrs = new ArrayList<>();
+                }
+                field.attrs.add(CODE);
+            });
+
+            node.methods.forEach(method -> {
+                if (method.attrs == null) {
+                    method.attrs = new ArrayList<>();
+                }
+                method.attrs.add(CONSTANT_VALUE);
+                method.attrs.add(STACKMAP);
+            });
+        }
         inst.setWorkDone();
+    }
+
+    private Attribute DummyAttribute(String name) {
+        Random random = new Random();
+        return DummyAttribute(name, new byte[2 + random.nextInt(4)]);
+    }
+
+    private Attribute DummyAttribute(String name, byte[] bytes) {
+        Random random = new Random();
+        random.nextBytes(bytes);
+        try {
+            Constructor<Attribute> constructor = Attribute.class.getDeclaredConstructor(String.class);
+            constructor.setAccessible(true);
+            Attribute attribute = constructor.newInstance(name);
+            Field content = attribute.getClass().getDeclaredField("content");
+            content.setAccessible(true);
+            content.set(attribute, bytes);
+            return attribute;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Boolean versionAtLeast(ClassNode node, int minVersion) {
+        int thisMajor = node.version & 0xFFFF;
+        int minMajor = minVersion & 0xFFFF;
+
+        return thisMajor >= minMajor;
     }
 
     @Override
